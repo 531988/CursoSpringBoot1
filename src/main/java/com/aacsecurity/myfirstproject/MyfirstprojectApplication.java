c36e602e25e39a12fa07a491d7beda12db446418
Alexsandro <alexs.alexsandrocosta@hotmail.com>
2022-09-13 11:03:40 -0300

    Product memory

--- src/main/java/com/aacsecurity/myfirstproject/MyfirstprojectApplication.java
+++ src/main/java/com/aacsecurity/myfirstproject/MyfirstprojectApplication.java
@@ -1,17 +1,24 @@
 package com.aacsecurity.myfirstproject;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.aacsecurity.myfirstproject.entities.Category;
+import com.aacsecurity.myfirstproject.entities.Product;
 import com.aacsecurity.myfirstproject.repositories.CategoryRepository;
+import com.aacsecurity.myfirstproject.repositories.ProductRepository;
 
 @SpringBootApplication
 public class MyfirstprojectApplication implements CommandLineRunner {
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(MyfirstprojectApplication.class, args);
@@ -22,8 +29,23 @@ public class MyfirstprojectApplication implements CommandLineRunner {
 		Category cat1 = new Category(1L, "Electronics");
 		Category cat2 = new Category(2L, "Books");
 
+		Product p1 = new Product(1L, "TV", 2200.00, cat1);
+		Product p2 = new Product(2L, "Domain Driver Design", 120.00, cat2);
+		Product p3 = new Product(3L, "PS5", 2800.00, cat1);
+		Product p4 = new Product(4L, "Docker", 100.00, cat2);
+		Product p5 = new Product(5L, "FIFA", 200.00, cat1);
+		
+		cat1.getProducts().addAll(Arrays.asList(p1,p3,p5));
+		cat2.getProducts().addAll(Arrays.asList(p2,p4));
+		
 		categoryRepository.save(cat1);
 		categoryRepository.save(cat2);
+		
+		productRepository.save(p1);
+		productRepository.save(p2);
+		productRepository.save(p3);
+		productRepository.save(p4);
+		productRepository.save(p5);
 	}
 
 }
